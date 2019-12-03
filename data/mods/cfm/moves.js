f96a34d6bcc89a8e729e3639b68377bf21f17f6c
Sharpest17 <33988339+Sharpest17@users.noreply.github.com>
2019-12-02 19:19:08 -0500

    Update moves.js
    natural gift

--- data/mods/cfm/moves.js
+++ data/mods/cfm/moves.js
@@ -12719,7 +12719,6 @@ let BattleMovedex = {
 			if (!item.naturalGift) return false;
 			move.basePower = item.naturalGift.basePower;
 			move.type = item.naturalGift.type;
-			pokemon.setItem('');
 			pokemon.lastItem = item.id;
 			pokemon.usedItemThisTurn = true;
 			this.runEvent('AfterUseItem', pokemon, null, null, item);
