a27cf0b7135794edfcd5c721b992f2160f408ea1
Nephrite <thechurchofcage@gmail.com>
2020-08-28 00:52:28 +0100

    Es-lint fixes, gave factory validator a title

--- test/sim/cfm/factory-validator.js
+++ test/sim/cfm/factory-validator.js
@@ -14,33 +14,33 @@ describe('CFM - Factory Validator', function () {
 		}
 	});
 	it('Validate all factory sets', function () {
-		let illegalSets = `\n`,
-		illegalSetsFound = false;
-		let tiers = {'Uber': 'ubers', 'OU': 'ou', 'UU': 'uu', 'RU': 'ru', 'NU': 'nu', 'PU': 'pu'};
+		let illegalSets = `\nCFM FACTORY ILLEGAL SETS\n\n`,
+			illegalSetsFound = false;
+		const tiers = {'Uber': 'ubers', 'OU': 'ou', 'UU': 'uu', 'RU': 'ru', 'NU': 'nu', 'PU': 'pu'};
 		for (const tier in factorySets) {
-			illegalSets += `\\\\ ${tier} \\\\\n\n`
+			illegalSets += `\\\\ ${tier} \\\\\n\n`;
 			for (const mon in factorySets[tier]) {
 				for (const subSet in factorySets[tier][mon]["sets"]) {
-					let rawSet			= Object.assign({}, factorySets[tier][mon]["sets"][subSet]);
-					let validateSets	= [];
+					const rawSet		= factorySets[tier][mon]["sets"][subSet];
+					const validateSets	= [];
 
 					for (const field of ['nature', 'item', 'ability']) {
 						if (typeof rawSet[field] === 'object') rawSet[field] = rawSet[field][0];
 					}
 
-					let moves = rawSet.moves.flat(),
-					i = 0;
+					const moves = rawSet.moves.flat();
+					let i = 0;
 					while (moves.length > i)
 					{
-						validateSets.push({ ...rawSet, moves: moves.splice(i, i + 4)});
+						validateSets.push({...rawSet, moves: moves.splice(i, i + 4)});
 						i += 4;
 					}
 
 					let team, illegal = null;
 					for (const validateSet of validateSets) {
-						team = [ validateSet ];
+						team = [validateSet];
 						illegal = TeamValidator.get(`gen8cfm${tiers[tier]}`).validateTeam(team);
-						if(illegal) {
+						if (illegal) {
 							illegalSetsFound = true;
 							illegalSets += `${tier} - ${mon} : ${illegal}\n`;
 						}
