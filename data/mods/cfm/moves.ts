75a765471d56590a1aae8c4f7cac87cb81dca497
Nephrite <thechurchofcage@gmail.com>
2021-03-14 22:34:21 +0000

    Snap Trap description update
    Mentions "omnitype"-ness

--- data/mods/cfm/moves.ts
+++ data/mods/cfm/moves.ts
@@ -15708,6 +15708,7 @@ Speed: BP varies depending on the relative speeds of user and target, like Elect
 		name: "Snap Trap",
 		desc: "Prevents the target from switching for four or five turns (seven turns if the user is holding Grip Claw). Causes damage to the target equal to 1/8 of its maximum HP (1/6 if the user is holding Binding Band), rounded down, at the end of each turn during effect. The target can still switch out if it is holding Shed Shell or uses Baton Pass, Parting Shot, Teleport, U-turn, or Volt Switch. The effect ends if either the user or the target leaves the field, or if the target uses Rapid Spin or Substitute successfully. This effect is not stackable or reset by using this or another binding move. The damage dealt each turn is absorbed by the user of the move, restoring their HP.",
 		shortDesc: "Traps and damages the target for 4-5 turns. Damage dealt heals the user.",
+		cfmDesc: "Changes to the user's primary type. Traps and damages the target for 4-5 turns. Damage dealt heals the user.",
 		pp: 15,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, omnitype: 1},
