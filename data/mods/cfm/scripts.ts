e4929ce547f8b0e60c6d05590612703292cd1cf8
Loris-1 <82619015+Loris-1@users.noreply.github.com>
2022-03-22 19:45:21 -0400

    fix trailing spaces and commas

--- data/mods/cfm/scripts.ts
+++ data/mods/cfm/scripts.ts
@@ -64,7 +64,7 @@ export const Scripts: CFMBattleScriptsData = {
 			suppressMessages = false
 		): number | undefined | null | false {
 			if (typeof move === 'string') move = this.dex.getActiveMove(move);
-	
+
 			if (typeof move === 'number') {
 				const basePower = move;
 				move = new Dex.Move({
@@ -75,13 +75,13 @@ export const Scripts: CFMBattleScriptsData = {
 				}) as ActiveMove;
 				move.hit = 0;
 			}
-	
+
 			if (!move.ignoreImmunity || (move.ignoreImmunity !== true && !move.ignoreImmunity[move.type])) {
 				if (!target.runImmunity(move.type, !suppressMessages)) {
 					return false;
 				}
 			}
-	
+
 			if (move.ohko) return target.maxhp;
 			if (move.damageCallback) return move.damageCallback.call(this.battle, source, target);
 			if (move.damage === 'level') {
@@ -89,16 +89,16 @@ export const Scripts: CFMBattleScriptsData = {
 			} else if (move.damage) {
 				return move.damage;
 			}
-	
+
 			const category = this.battle.getCategory(move, source);
-	
+
 			let basePower: number | false | null = move.basePower;
 			if (move.basePowerCallback) {
 				basePower = move.basePowerCallback.call(this.battle, source, target, move);
 			}
 			if (!basePower) return basePower === 0 ? undefined : basePower;
 			basePower = this.battle.clampIntRange(basePower, 1);
-	
+
 			let critMult;
 			let critRatio = this.battle.runEvent('ModifyCritRatio', source, target, move, move.critRatio || 0);
 			if (this.battle.gen <= 5) {
@@ -112,7 +112,7 @@ export const Scripts: CFMBattleScriptsData = {
 					critMult = [0, 24, 8, 2, 1];
 				}
 			}
-	
+
 			const moveHit = target.getMoveHitData(move);
 			moveHit.crit = move.willCrit || false;
 			if (move.willCrit === undefined) {
@@ -120,45 +120,45 @@ export const Scripts: CFMBattleScriptsData = {
 					moveHit.crit = this.battle.randomChance(1, critMult[critRatio]);
 				}
 			}
-	
+
 			if (moveHit.crit) {
 				moveHit.crit = this.battle.runEvent('CriticalHit', target, null, move);
 			}
-	
+
 			// happens after crit calculation
 			basePower = this.battle.runEvent('BasePower', source, target, move, basePower, true);
-	
+
 			if (!basePower) return 0;
 			basePower = this.battle.clampIntRange(basePower, 1);
 			// Hacked Max Moves have 0 base power, even if you Dynamax
 			if ((!source.volatiles['dynamax'] && move.isMax) || (move.isMax && this.dex.moves.get(move.baseMove).isMax)) {
 				basePower = 0;
 			}
-	
+
 			const level = source.level;
-	
+
 			const attacker = move.overrideOffensivePokemon === 'target' ? target : source;
 			const defender = move.overrideDefensivePokemon === 'source' ? source : target;
 
 			const isPhysical = category === 'Physical';
 			let attackStat: StatIDExceptHP = move.overrideOffensiveStat ? (isPhysical ? 'def' : 'spd') : (isPhysical ? 'atk' : 'spa');
 			const defenseStat: StatIDExceptHP = move.overrideDefensiveStat || (isPhysical ? 'def' : 'spd');
-	
+
 			const statTable = {atk: 'Atk', def: 'Def', spa: 'SpA', spd: 'SpD', spe: 'Spe'};
-	
+
 			let atkBoosts = attacker.boosts[attackStat];
 			let defBoosts = defender.boosts[defenseStat];
-	
+
 			let ignoreNegativeOffensive = !!move.ignoreNegativeOffensive;
 			let ignorePositiveDefensive = !!move.ignorePositiveDefensive;
-	
+
 			if (moveHit.crit) {
 				ignoreNegativeOffensive = true;
 				ignorePositiveDefensive = true;
 			}
 			const ignoreOffensive = !!(move.ignoreOffensive || (ignoreNegativeOffensive && atkBoosts < 0));
 			const ignoreDefensive = !!(move.ignoreDefensive || (ignorePositiveDefensive && defBoosts > 0));
-	
+
 			if (ignoreOffensive) {
 				this.battle.debug('Negating (sp)atk boost/penalty.');
 				atkBoosts = 0;
@@ -167,25 +167,25 @@ export const Scripts: CFMBattleScriptsData = {
 				this.battle.debug('Negating (sp)def boost/penalty.');
 				defBoosts = 0;
 			}
-	
+
 			let attack = attacker.calculateStat(attackStat, atkBoosts);
 			let defense = defender.calculateStat(defenseStat, defBoosts);
-	
+
 			attackStat = (category === 'Physical' ? 'atk' : 'spa');
-	
+
 			// Apply Stat Modifiers
 			attack = this.battle.runEvent('Modify' + statTable[attackStat], source, target, move, attack);
 			defense = this.battle.runEvent('Modify' + statTable[defenseStat], target, source, move, defense);
-	
+
 			if (this.battle.gen <= 4 && ['explosion', 'selfdestruct'].includes(move.id) && defenseStat === 'def') {
 				defense = this.battle.clampIntRange(Math.floor(defense / 2), 1);
 			}
-	
+
 			const tr = this.battle.trunc;
-	
+
 			// int(int(int(2 * L / 5 + 2) * A * P / D) / 50);
 			const baseDamage = tr(tr(tr(tr(2 * level / 5 + 2) * basePower * attack) / defense) / 50);
-	
+
 			// Calculate damage modifiers separately (order differs between generations)
 			return this.modifyDamage(baseDamage, source, target, move, suppressMessages);
 		},
@@ -612,6 +612,6 @@ export const Scripts: CFMBattleScriptsData = {
 				this.battle.add('-immune', this);
 			}
 			return false;
-		}
-	}
+		},
+	},
 };
